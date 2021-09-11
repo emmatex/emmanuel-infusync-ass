63424b91dc932142924ddf468d7e7d0b323a6c9d
Emmatex <emmatex4yall@hotmail.com>
2021-09-12 00:11:04 +0100

    controllers

---
+++ src/API/Controllers/RoomsController.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using System;
+
+namespace API.Controllers
+{
+    public class RoomsController : BaseController
+    {
+        private readonly IGenericRepository<Room> _repository;
+
+        public RoomsController(IGenericRepository<Room> repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+    }
+}
